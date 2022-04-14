012eec1310ca23929518fd722113505e357268d8
Sophie Luna Schumann <me@luna.vg>
2022-04-14 14:31:40 +0200

    now works like the CF importer

--- terracloudns-dump.go
+++ terracloudns-dump.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"git.catgirl.biz/sophie/goterracloudns/v2/cloudns"
@@ -13,13 +14,19 @@ import (
 var authID = flag.Int("sub-auth-id", 0, "Sub Auth ID")
 var authPassword = flag.String("auth-password", "", "Auth Password")
 var zoneFlag = flag.String("zone", "", "Zone name")
+var outPath = flag.String("out", "import.tf", "Output file path")
 
 func main() {
 	flag.Parse()
-	if *authID <= 0 || *authPassword == "" || *zoneFlag == "" {
+	if *authID <= 0 || *authPassword == "" || *zoneFlag == "" || *outPath == "" {
 		flag.Usage()
 		return
 	}
+
+	fd, err := os.OpenFile(*outPath, os.O_WRONLY|os.O_CREATE, 0644)
+	defer fd.Close()
+	writer := bufio.NewWriter(fd)
+	defer writer.Flush()
 
 	api := cloudns.New(strconv.Itoa(*authID), strings.Clone(*authPassword))
 	loginStatus, err := api.Login()
@@ -44,12 +51,12 @@ func main() {
 	}
 	fmt.Printf("Reading zone records for %s and converting to Terraform...\n", zone.Name)
 
-	tpl, err := template.New("tf_dns_record").Parse(`resource "cloudns_dns_record" "terraform_managed_record_{{.ID}}" {
+	tpl, err := template.New("tf_dns_record").Parse(`resource "cloudns_dns_record" "{{.SafeID}}_{{.ID}}" {
   # {{.Host}}.{{.Zone}} {{.TTL}} in {{.Type}} {{.Record}}
   name  = "{{.Host}}"
   zone  = "{{.Zone}}"
   type  = "{{.Type}}"
-  value = "{{.Record}}"
+  value = "{{.SafeRecord}}"
   ttl   = "{{.TTL}}"
 }`)
 	if err != nil {
@@ -58,23 +65,29 @@ func main() {
 
 	records, err := api.GetRecordsForZone(zone.Name)
 	for _, record := range records {
+		safeID := strings.Replace(zone.Name, ".", "_", -1)
 		vals := struct {
-			ID     string
-			Zone   string
-			Host   string
-			Type   string
-			Record string
-			TTL    string
+			SafeID     string
+			ID         string
+			Zone       string
+			Host       string
+			Type       string
+			Record     string
+			SafeRecord string
+			TTL        string
 		}{
-			ID:     record.ID,
-			Zone:   zone.Name,
-			Host:   record.Host,
-			Type:   record.Type,
-			Record: record.Record,
-			TTL:    record.TTL,
+			SafeID:     safeID,
+			ID:         record.ID,
+			Zone:       zone.Name,
+			Host:       record.Host,
+			Type:       record.Type,
+			Record:     record.Record,
+			SafeRecord: strings.Replace(record.Record, "\"", "\\\"", -1),
+			TTL:        record.TTL,
 		}
-		err := tpl.Execute(os.Stdout, vals)
-		os.Stdout.WriteString("\n\n")
+		err := tpl.Execute(writer, vals)
+		writer.WriteString("\n\n")
+		fmt.Printf("terraform import cloudns_dns_record.%s_%s cloudns_dns_record/%s_%s\n", safeID, record.ID, safeID, record.ID)
 		if err != nil {
 			panic(err)
 		}
